66e236dce080410ae0506b1e5a16585c16b05a82
Global Name <chinenyevans@gmail.com>
2026-05-12 12:28:55 +0100

    "Edit DB to be found by render"

--- src/retrieve.py
+++ src/retrieve.py
@@ -1,8 +1,42 @@
-from src.embed import load_vector_store
-#from rank_bm25 import BM25Okapi
-#import pickle
+import os
+
+from src.embed import (
+    load_vector_store,
+    create_vector_store
+)
+
+from src.ingest import (
+    load_documents,
+    split_documents
+)
+
+from src.config import VECTOR_DB_PATH
 
 
 def get_retriever():
-    db = load_vector_store()
-    return db.as_retriever(search_kwargs={"k": 8})
+
+    index_file = os.path.join(
+        VECTOR_DB_PATH,
+        "index.faiss"
+    )
+
+    # If DB does not exist, build it
+    if not os.path.exists(index_file):
+
+        print("Vector DB not found. Building index...")
+
+        documents = load_documents()
+
+        chunks = split_documents(documents)
+
+        db = create_vector_store(chunks)
+
+    else:
+
+        print("Loading existing vector DB...")
+
+        db = load_vector_store()
+
+    return db.as_retriever(
+        search_kwargs={"k": 4}
+    )
